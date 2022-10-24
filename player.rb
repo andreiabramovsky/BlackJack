b93aa281b9f5d7d91921c87d7f30bdaf4189af59
andreiabramovsky <andreyabramovsky@gmail.com>
2022-10-24 22:30:45 +0300

    add methods

--- player.rb
+++ player.rb
@@ -6,7 +6,30 @@
 # может делать ставку, брать карты на руку, считать количество очков, пропускать ход, показывать количество очков, показывать карты на руке
 
 class Player
+
   def initialize(name)
     @name = name
+    @bank = bank
+    @hand = []
+    @points = []
   end
+
+  def make_bet
+  end
+
+  def take_card
+  end
+
+  def count_points
+  end
+
+  def skip
+  end
+
+  def show_points
+  end
+
+  def show_cards
+  end
+  
 end
